19ccf94e32c6e2cb1a71082ffce76cafdf2b0d7a
DESKTOP-FED0M0J\LENOVO <msmannan00@gmail.com>
2018-08-08 11:00:55 -0700

    local string seperated in constant file

---
+++ src/torcrawler/Constants.java
@@ -0,0 +1,13 @@
+package torcrawler;
+
+public class Constants {
+    /*Tor Request Handler*/
+    public static String proxyIP = "127.0.0.1";
+    public static String testOnionURL = "https://securityzap.com/massive-deep-web-links-2015-updated-june-2015/";
+    public static String urlExtractingRegex = "((https?|ftp|gopher|telnet|file):((//)|(\\\\))+[\\w\\d:#@%/;$()~_?\\+-=\\\\\\.&]*)";
+    public static String errorLogs = "CODE ERROR ";
+    public static String emptyString = "";
+    
+    public static int proxyPort = 9150;
+    public static int emptyInt = 0;
+}
--- src/torcrawler/TorRequestHandler.java
+++ src/torcrawler/TorRequestHandler.java
@@ -6,26 +6,57 @@ import java.net.Proxy;
 import java.net.SocketAddress;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TorRequestHandler 
 {
     public void pingOnionLink() {
         
         try {
-            SocketAddress addr = new InetSocketAddress("127.0.0.1", 9150);
+            SocketAddress addr = new InetSocketAddress(Constants.proxyIP,Constants.proxyPort);
             Proxy proxy = new Proxy(Proxy.Type.SOCKS, addr);      
-            URL url = new URL("http://kbvbh4kdddiha2ht.onion/");
+            URL url = new URL(Constants.testOnionURL);
             URLConnection conn = url.openConnection(proxy);
             
             BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             String inputLine;
+            String HtmlCode = Constants.emptyString;
             while ((inputLine = in.readLine()) != null) 
-                System.out.println(inputLine);
+                HtmlCode = HtmlCode + inputLine;
             in.close();
+            
+            /*KEY WORDS*/
+            /*HEADER*/
+            /*SUMMARY*/
+            /*IMAGES LINKS*/
+            /*VIDEO LINKS*/
+            /*DOWNLOADS AND TOR LINKS*/
+            /*BOOKS AND DOCUMENTS*/
+            /*UPLOAD TO DATABASE*/
+            
+            System.out.println("TOTAL FOUND LINKS : " + extractUrls(HtmlCode).size());
         } 
         catch (Exception ex) {
-            System.out.println("CODE ERROR " + ex.toString());
+            System.out.println(Constants.errorLogs + ex.toString());
         }
    }
     
+    public static List<String> extractUrls(String text) {
+
+        List<String> containedUrls = new ArrayList<String>();
+        String urlRegex = Constants.urlExtractingRegex;
+        Pattern pattern = Pattern.compile(urlRegex, Pattern.CASE_INSENSITIVE);
+        Matcher urlMatcher = pattern.matcher(text);
+
+        while (urlMatcher.find())
+        {
+                containedUrls.add(text.substring(urlMatcher.start(Constants.emptyInt),
+                urlMatcher.end(Constants.emptyInt)));
+        }
+        return containedUrls;
+    }
 }
+ 
